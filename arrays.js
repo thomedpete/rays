6e15cda6e90bfd238fee2c214cbb85e6474f50ba
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-06-08 11:26:53 -0600

    Added methods

--- arrays.js
+++ arrays.js
@@ -1,7 +1,12 @@
 var strings = ["s1","s2","s3","s4"]
+strings.shift()
+//gets rid of first element
 
-
-var number = [10,11,12,13]
+var numbers = [10,11,12,13]
+numbers.unshift(9)
+//adds element to the front of the array
 
 
 var boo = [true,false,true,false]
+boo.pop()
+//it takes out the last element 
