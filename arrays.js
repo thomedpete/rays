dc07eae8d825e40c663bad5a03936bfd484a765e
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-06-08 11:30:57 -0600

    Added explanation

--- arrays.js
+++ arrays.js
@@ -1,6 +1,8 @@
 var strings = ["s1","s2","s3","s4"]
 strings.shift()
 //gets rid of first element
+console.log(strings[2])
+//the above log will return s3   0,1,2,3
 
 var numbers = [10,11,12,13]
 numbers.unshift(9)
@@ -9,4 +11,4 @@ numbers.unshift(9)
 
 var boo = [true,false,true,false]
 boo.pop()
-//it takes out the last element 
+//it takes out the last element
